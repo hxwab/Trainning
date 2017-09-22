72cd9b109bdfa555d038ebec93a75c910ad3d6bc
hxwab <hxwab2010@126.com>
2017-09-22 23:33:54 +0800

    add

--- My/src/com/thread/TestRunnable.java
+++ My/src/com/thread/TestRunnable.java
@@ -5,16 +5,16 @@ public class TestRunnable implements Runnable {
 	@Override
 	public void run() {
 		System.out.println(Thread.currentThread().getName()+"线程被调用了");
-		while(true){
+		
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(2);
 				System.out.println(Thread.currentThread().getName());
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-		}
+		
 	}
 
 }
--- My/src/com/thread/ThreadPoolExecutor.java
+++ My/src/com/thread/ThreadPoolExecutor.java
@@ -2,6 +2,8 @@ package com.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutor {
 	
@@ -12,10 +14,19 @@ public class ThreadPoolExecutor {
 		ExecutorService executor3 = Executors.newScheduledThreadPool(10);
 		for(int i=0;i<5;i++){
 			executor.execute(new TestRunnable());
-		    System.out.println("******a"+i+"******");
+		    System.out.println("a"+i+"******");
 		}
 		executor.shutdown();
+		testThreadPoolExecutor();
+	}
+	
+	public static  void testThreadPoolExecutor(){
 		
+		java.util.concurrent.ThreadPoolExecutor executor = new java.util.concurrent.ThreadPoolExecutor(10, 20, 1000, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(10));
+		for(int i=0;i<50;i++){
+			executor.execute(new TestRunnable());
+		    System.out.println("******a***"+i+"******");
+		}
 	}
 
 }
